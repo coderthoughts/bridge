0aa95bb26a13a0fb435ad34e8cedb58cd701ea39
David Bosschaert <bosschae@adobe.com>
2016-02-06 20:51:27 +0000

    Run in a thread

--- client/src/main/java/org/coderthoughts/bridge/client/PrimeClient.java
+++ client/src/main/java/org/coderthoughts/bridge/client/PrimeClient.java
@@ -3,6 +3,7 @@ package org.coderthoughts.bridge.client;
 import org.coderthoughts.bridge.impl.PrimeNumbers;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 @Component
@@ -10,12 +11,28 @@ public class PrimeClient {
     @Reference
     private PrimeNumbers primeGenerator;
 
+    private volatile boolean keepRunning = false;
+
     @Activate
     private void start() {
-        System.out.println("First 10 primes:");
-        for (int i=0, p=1; i<10; i++) {
-            p = primeGenerator.nextPrime(p);
-            System.out.println(p);
-        }
+        keepRunning = true;
+        new Thread(() -> {
+            while (keepRunning) {
+                System.out.print("First 10 primes: ");
+                for (int i=0, p=1; i<10; i++) {
+                    if (i > 0)
+                        System.out.print(", ");
+                    p = primeGenerator.nextPrime(p);
+                    System.out.print(p);
+                }
+                System.out.println();
+                try { Thread.sleep(1000); } catch (InterruptedException ie) {}
+            }
+        }).start();
+    }
+
+    @Deactivate
+    private void stop() {
+        keepRunning = false;
     }
 }
