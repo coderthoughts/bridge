56344e34e1fd0766a0c2f811d5cfbe5901af51c6
David Bosschaert <bosschae@adobe.com>
2016-02-06 21:15:08 +0000

    Add providertype annotation

--- service/src/main/java/org/coderthoughts/bridge/service/PrimeNumberService.java
+++ service/src/main/java/org/coderthoughts/bridge/service/PrimeNumberService.java
@@ -1,5 +1,8 @@
 package org.coderthoughts.bridge.service;
 
+import org.osgi.annotation.versioning.ProviderType;
+
+@ProviderType
 public interface PrimeNumberService {
     int nextPrime(int n);
 }
