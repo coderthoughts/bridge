7762a76aa5b3b79f4425aee4228716b3cc74d646
David Bosschaert <bosschae@adobe.com>
2016-02-06 20:53:23 +0000

    Add thread

--- client/src/main/java/org/coderthoughts/bridge/client/PrimeClient.java
+++ client/src/main/java/org/coderthoughts/bridge/client/PrimeClient.java
@@ -6,11 +6,19 @@ public class PrimeClient {
     private PrimeNumbers primeGenerator = new PrimeNumbers();
 
     private void start() {
-        System.out.println("First 10 primes:");
-        for (int i=0, p=1; i<10; i++) {
-            p = primeGenerator.nextPrime(p);
-            System.out.println(p);
-        }
+        new Thread(() -> {
+            while (true) {
+                System.out.print("First 10 primes: ");
+                for (int i=0, p=1; i<10; i++) {
+                    if (i > 0)
+                        System.out.print(", ");
+                    p = primeGenerator.nextPrime(p);
+                    System.out.print(p);
+                }
+                System.out.println();
+                try { Thread.sleep(1000); } catch (InterruptedException ie) {}
+            }
+        }).start();
     }
 
     public static void main(String[] args) {
